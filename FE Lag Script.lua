707d4ee4571b57e2135f5cb08f56125943637a42
Ultimatetinycellstuan <92477885+Ultimatetinycellstuan@users.noreply.github.com>
2022-10-14 11:53:29 -0500

    Update FE Lag Script.lua

--- FE Lag Script.lua
+++ FE Lag Script.lua
@@ -1,26 +1,26 @@
 sounds = {}
 
 function getSounds(loc)
-if loc:IsA("Sound") then
+  if loc:IsA("Sound") then
 table.insert(sounds,loc)
 end
-for _,obj in pairs(loc:GetChildren()) do
-getSounds(obj)
-end
-end
+  for _,obj in pairs(loc:GetChildren()) do
+    getSounds(obj)
+  end
+end)
 
 getSounds(game)
 
 game.DescendantAdded:connect(function(obj)
-if obj:IsA("Sound") then
+  if obj:IsA("Sound") then
 table.insert(sounds,obj)
-end
+   end
 end)
 
 while wait(0.2) do
-for _,sound in pairs(sounds) do
-pcall(function()
-sound:Play()
+  for _,sound in pairs(sounds) do
+      pcall(function()
+     sound:Play()
+    end
+  end
 end)
-end
-end
